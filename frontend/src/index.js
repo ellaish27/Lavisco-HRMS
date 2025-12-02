58c6fe63abe0be75a68eef0bc9e1d482b21b4773
D@in Corp <ellaish27@gmail.com>
2025-12-02 17:22:24 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 // frontend/src/index.js
-import React from 'cute';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
